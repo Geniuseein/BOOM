8c52decf3ce56edc3e445243bc8c355aa466d7c4
YimoSheny <Geniuseein@gmail.com>
2025-05-15 14:40:30 +0800

    add hold table

--- exu/rename/rename-holdtable.scala
+++ exu/rename/rename-holdtable.scala
@@ -6,7 +6,7 @@ import boom.common._
 import boom.util._
 import freechips.rocketchip.config.Parameters
 
-class ReHoldReq(val pregSz: Int) extends Bundle
+class HoldReq(val pregSz: Int) extends Bundle
 {
   val prs1 = Valid(UInt(pregSz.W))
   val prs2 = Valid(UInt(pregSz.W))
@@ -23,17 +23,55 @@ class RenameHoldTable(
   val pregSz = log2Ceil(numPregs)
 
   val io = IO(new BoomBundle()(p) {
-    val hold_resps = Output(Vec(plWidth, Bool())) // Whether the stale physical register is busy
-    val hold_reqs = Input(Vec(plWidth, UInt(pregSz.W))) // stale physical register
+    val query_resps = Output(Vec(plWidth, Bool())) // Whether the stale physical register is busy
+    val query_reqs = Input(Vec(plWidth, UInt(pregSz.W))) // stale physical register
 
-    val rehold_reqs = Input(Vec(plWidth, new ReHoldReq(pregSz.W)))
+    val hold_reqs = Input(Vec(plWidth, new HoldReq(pregSz.W)))
 
-    val reset_pregs = Input(Vec(plWidth, Valid(UInt(pregSz.W)))) // early release of stale physical register
+    val early_rls_pregs = Input(Vec(plWidth, Valid(UInt(pregSz.W)))) // early release of stale physical register
     val dealloc_pregs = Input(Vec(plWidth, Valid(UInt(pregSz.W)))) // pregs returned by the ROB
     
-    val brupdate = Input(new BrUpdateInfo)
+    val recovering = Input(Bool()) // whether the pipeline is recovering from a misprediction
   })
 
-  val hold
+  val hold_table = RegInit(0.U(numPregs.W))
 
+  // 1. clear bits for early releases
+  val hold_table_rls = hold_table & ~io.early_rls_pregs
+    .map { rls =>
+      UIntToOH(rls.bits, numPregs) & Fill(numPregs, rls.valid.asUInt)
+    }.reduce(_|_)
+
+  // 2. clear bits for deallocations
+  val hold_table_dealloc = hold_table_rls & ~io.dealloc_pregs
+    .map { dealloc =>
+      UIntToOH(dealloc.bits, numPregs) & Fill(numPregs, dealloc.valid.asUInt)
+    }.reduce(_|_)
+
+  // 3. set bits for new hold_reqs (prs1, prs2, prs3)
+  val hold_table_next = hold_table_dealloc | io.hold_reqs
+    .flatMap(req => Seq(req.prs1, req.prs2, req.prs3))
+    .map { v =>
+      UIntToOH(v.bits, numPregs) & Fill(numPregs, v.valid.asUInt)
+    }.reduce(_|_)
+
+  
+
+  when (!io.recovering) {
+    hold_table := hold_table_next
+  } .otherwise {
+    // If recovering, clear the hold table
+    hold_table := 0.U(numPregs.W)
+  }
+
+  // 4. query_reqs: output hold_table status for each query
+  for (i <- 0 until plWidth) {
+    val was_bypassed = (0 until i).map { j =>
+      io.query_reqs(i) === io.hold_reqs(j).prs1.bits && io.hold_reqs(j).prs1.valid ||
+      io.query_reqs(i) === io.hold_reqs(j).prs2.bits && io.hold_reqs(j).prs2.valid ||
+      io.query_reqs(i) === io.hold_reqs(j).prs3.bits && io.hold_reqs(j).prs3.valid
+    }.foldLeft(false.B)(_||_)
+
+    io.query_resps(i) := hold_table(io.query_reqs(i)) || (was_bypassed && bypass.B)
+  }
 }
